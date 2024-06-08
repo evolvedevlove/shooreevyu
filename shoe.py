b675144c9b48f2462ccf1dba23c986ea5f67add6
evolvedevlove <pattyworkshardforthemoney@gmail.com>
2024-06-07 22:45:12 -0600

    added placeholder text- now can start on the layout and db io

--- shoe.py
+++ shoe.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/index.html')
 def index():
-    return '<p>we are here</p>'
+    return '<p>we are here</p><p>but go <a href="reviews.html">here</a> if you want</p>'
+
+@app.route('/reviews.html')
+def reviews():
+    html_head = f"<head><link rel=stylesheet type=css</head>" # need a stylesheet and ? to use db connection    
+    first_review_text = "the dc white shoes were great but grip under the left ball wore through in 4 months"
+    html_body = f"<body>{first_review_text}</body>" 
+    
+    return html_head + html_body
 
 @app.route('/')
 def hey_shoo_viewer():
