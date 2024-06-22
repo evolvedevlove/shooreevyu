1bdf6fb579b3ece9dc90013ddb467f2c1e9f98c6
evolvedevlove <pattyworkshardforthemoney@gmail.com>
2024-06-21 21:52:13 -0600

    not much but a little more of a review

--- shoe.py
+++ shoe.py
@@ -11,11 +11,16 @@ def index():
 def reviews():
     html_head = f"<head><link rel=stylesheet type=css</head>" # need a stylesheet and ? to use db connection    
     first_review_text = "the dc white shoes were great but grip under the left ball wore through in 4 months"
-    html_body = f"<body>{first_review_text}</body>" 
+    second_review_text = "at long last the white cons are sick a shoe that I could ollie and switch ollie "
+    html_body = f"<body><ul><li>{first_review_text}</li><li>{second_review_text}</li></body>" 
     # TO DO - template page for reviews is a 4 image gallery with a paragraph below
     # what else goes in a review ? title, date, rating, post
 
     return html_head + html_body
+
+@app.route('/contact.html')
+def contact():
+    text = f"not sure what you want to get a hold of me but here is the place to do it."
 
 @app.route('/')
 def hey_shoo_viewer():
